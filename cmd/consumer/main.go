6f46d65681b1c4fd0ca3a61395d090b2f61f298d
Gifary Dhimas Fadhillah <gifarydhimas@gmail.com>
2021-12-12 20:25:20 +0700

    feat: allow multiple consumer spawn

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"os/signal"
 	"strconv"
+	"sync"
 	"syscall"
 	"time"
 
@@ -16,52 +17,88 @@ import (
 )
 
 var (
-	logLevel  string
-	serverURI string
-	userID    int
+	logLevel      string
+	serverURI     string
+	userID        int
+	numOfConsumer int
 )
 
 func main() {
 	flag.StringVar(&logLevel, "log-level", "INFO", "log level. Available options: DEBUG, INFO, WARN, DEBUG")
 	flag.StringVar(&serverURI, "server-uri", "", "websocket server URI. i.e: ws://localhost:8080/messages/listen")
 	flag.IntVar(&userID, "user-id", 1, "user id to be embed in X-User-Id header")
+	flag.IntVar(&numOfConsumer, "n", 1, "number of consumers to be spawned")
 	flag.Parse()
 
 	log.SetOutput(logger.NewLevelFilter(logLevel, os.Stdout))
 
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+	spawnConsumer(numOfConsumer, quit)
+}
+
+func spawnConsumer(n int, quit <-chan os.Signal) {
+	if n < 1 {
+		panic("number of consumer must be positive")
+	}
+
+	wg := sync.WaitGroup{}
+	wg.Add(n)
+	stopChan := make(chan struct{}, n)
+
+	go func() {
+		quitSig := <-quit
+		log.Printf("[DEBUG] quit signal acknowledged: signal=%s", quitSig)
+		log.Printf("[INFO] shutting down consumers")
+		for i := 0; i < n; i++ {
+			stopChan <- struct{}{}
+		}
+	}()
+
 	requestHeader := http.Header{}
 	requestHeader.Set("X-User-Id", strconv.Itoa(userID))
 
-	wsClient := wsclient.NewClient(serverURI, requestHeader)
-	wsClient.AttachObserver(consumer)
-	errChan, err := wsClient.Listen()
-	if err != nil {
-		log.Printf("[ERROR] unable to listen: %s", err)
-		return
-	}
-
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
-
-	select {
-	case <-quit:
-	case err := <-errChan:
-		opErr, ok := err.(*net.OpError)
-		if ok && opErr.Unwrap() == net.ErrClosed {
-			log.Printf("[DEBUG] unable to read message due to connection closed")
-		} else {
-			log.Printf("[ERROR] error while reading message: %s", err)
+	for i := 0; i < n; i++ {
+		wsClient := wsclient.NewClient(serverURI, requestHeader)
+		wsClient.AttachObserver(createMessageConsumer(i))
+		errChan, err := wsClient.Listen()
+		if err != nil {
+			log.Printf("[ERROR][Consumer: %d] unable to listen: %s", i, err)
+			wg.Done()
+			continue
 		}
+
+		go func(wsClient *wsclient.Client, wg *sync.WaitGroup, consumerNum int) {
+			select {
+			case <-stopChan:
+			case err := <-errChan:
+				if err != nil {
+					opErr, ok := err.(*net.OpError)
+					if ok && opErr.Unwrap() == net.ErrClosed {
+						log.Printf("[DEBUG][Consumer: %d] unable to read message due to connection closed", consumerNum)
+					} else {
+						log.Printf("[ERROR][Consumer: %d] error while reading message: %s", consumerNum, err)
+					}
+				}
+			}
+
+			err := wsClient.Stop(3 * time.Second)
+			if err != nil {
+				log.Printf("[ERROR][Consumer: %d] error when closing connection: %s", consumerNum, err)
+			}
+			log.Printf("[DEBUG][Consumer: %d] consumer has been shut down", consumerNum)
+
+			wg.Done()
+		}(wsClient, &wg, i)
+
 	}
 
-	log.Printf("[INFO] shutting down consumer")
-
-	err = wsClient.Stop(3 * time.Second)
-	if err != nil {
-		log.Printf("[ERROR] error when closing connection: %s", err)
-	}
+	wg.Wait()
+	log.Printf("[DEBUG] all consumers are shut down")
 }
 
-func consumer(msgType int, msg []byte) {
-	log.Printf("[DEBUG] consumed message: %s", msg)
+func createMessageConsumer(consumerNum int) wsclient.ObserverFunc {
+	return func(msgType int, msg []byte) {
+		log.Printf("[DEBUG][Consumer: %d] consumed message: %s", consumerNum, msg)
+	}
 }
